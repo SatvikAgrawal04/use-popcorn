c3eb5033fc1be7d189798765f5eab90582cdd4c4
Satvik Agrawal <satwikagrawal@gmail.com>
2023-09-22 11:27:59 +0530

    further segregated components

--- src/App.js
+++ src/App.js
@@ -6,55 +6,9 @@ import WatchedList from "./Components/WatchedList";
 import Summary from "./Components/Summary";
 import Loader from "./Components/Loader";
 import Search from "./Components/Search";
+import Logo from "./Components/Logo";
+import NumResults from "./Components/NumResults";
 
-const tempMovieData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt0133093",
-		Title: "The Matrix",
-		Year: "1999",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt6751668",
-		Title: "Parasite",
-		Year: "2019",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-	},
-];
-
-const tempWatchedData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-		runtime: 148,
-		imdbRating: 8.8,
-		userRating: 10,
-	},
-	{
-		imdbID: "tt0088763",
-		Title: "Back to the Future",
-		Year: "1985",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-		runtime: 116,
-		imdbRating: 8.5,
-		userRating: 9,
-	},
-];
-const average = arr =>
-	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 export default function App() {
 	const [movie, setMovie] = useState([]);
 	const [movies, setMovies] = useState([]);
@@ -147,23 +101,6 @@ export default function App() {
 
 function Navbar({ children }) {
 	return <nav className="nav-bar">{children}</nav>;
-}
-
-function NumResults({ movies }) {
-	return (
-		<p className="num-results">
-			Found <strong>{movies.length}</strong> results
-		</p>
-	);
-}
-
-function Logo() {
-	return (
-		<div className="logo">
-			<span role="img">🍿</span>
-			<h1>usePopcorn</h1>
-		</div>
-	);
 }
 
 function Main({ children }) {
